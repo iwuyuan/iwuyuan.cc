24b4548e584704095d05da0a407c4ffab4e3c929
醉美婺源 <18720358338@163.com>
2016-12-17 18:59:52 +0800

    test
    本地同步

--- js/site.js
+++ js/site.js
@@ -9,19 +9,31 @@
 $(document).ready(function(){
 	// ****************** 首页 ********************
 	// 第一项组图标头高度+导航栏高度
-	var _pageHeaderFirst = innerHeight - $(".wuyuan_beauty .page-header").height()-60 +"px";
-	$(".first_full").css("height",_pageHeaderFirst);
-	//顶部区域显示的高度
-	$(".first_full p").animate({height:_pageHeaderFirst}, "slow");
+	// var _pageHeaderFirst = innerHeight - $(".wuyuan_beauty .page-header").height()-60 +"px";
+	// $(".first_full").css("height",_pageHeaderFirst);
+	// //顶部区域显示的高度
+	// $(".first_full p").animate({height:_pageHeaderFirst}, "slow");
 	// 初始化图片查看器插件
 	// $(".wuyuan_spring").viewer({url: 'data-src'});
 	// **********************************
-	
+	// 手机版菜单栏展开时点击导航栏以外区域（不包括footer）收起菜单栏
+	$("section").off("click").on("click",function(){
+		if($(".navbar-collapse").hasClass("in")){
+			$(".navbar-toggle").trigger("click");
+		}
+	});
 	// 显示隐藏回到顶部
 	CheckToTop();
 	window.onscroll=function(){
 		CheckToTop();
 	}
+	// 缓慢滑动回到顶部
+	$(".back_top").off("click").on("click",function(){
+		$('html,body').animate({
+	        scrollTop:"0"
+	    }, 1000);
+	});
+	// 提示信息
 	$(".iwuyuan_pic img,.iwuyuan_article a,.page_container a").off("click").on("click",function(){
 		layer.msg('程序员宝宝还没写好<br/>先看看别的吧', {
 	    	time: 4000, //4s后自动关闭
